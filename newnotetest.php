0874ba7c60fcf1555bbb2a7ef185f658629b745e
sam4343 <69377272+sam4343@users.noreply.github.com>
2022-10-21 23:38:05 +1100

    Comments

--- newnotetest.php
+++ newnotetest.php
@@ -2,26 +2,26 @@
     include_once("docs/database/db_conn.php");
     print_r($_POST);
 
+	$message = htmlspecialchars($_POST['origin']); //Get message
+    $iv = htmlspecialchars($_POST['iv']); //Get IV length for encryption
 
-
-	$message = htmlspecialchars($_POST['origin']);
-    $iv = htmlspecialchars($_POST['iv']);
-
+    //Check that message exists, if not it redirects back to homepage
     if(!isset($_POST['message'])){
          header("HTTP/1.1 400 Bad Request");
     }
+
     // check whether message changed
     if(hash('sha256',utf8_encode($_POST['origin'])) != $_POST['hash']){
         header("HTTP/1.1 400 message changes");
-        console_log("Result: message has been changed");
+        console_log("Result: message has been changed"); //Put result in console
      
     }else{
     $query = "INSERT INTO `notes` (`message`) VALUES ('$message')";
-	$result = $conn->query($query);
+	$result = $conn->query($query); //Insert message into database
 	
-    console_log("Result: ".$result);
+    console_log("Result: ".$result); //Put result in console
     }
-	mysqli_close($conn);
+	mysqli_close($conn); //Close connection to database
 
     // Console function
     function console_log($output, $with_script_tags = true) {
